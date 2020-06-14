cb116368dacbfa22920804951f85e401bf0c254b
Yanier alfonso <48162982+elyanio@users.noreply.github.com>
2020-06-14 19:53:53 -0400

    add name and description in the list

--- components/UserListView.tsx
+++ components/UserListView.tsx
@@ -11,6 +11,8 @@ export interface MockData {
 interface Props {
     width: number
     height: number
+    nameRepo: string,
+    descriptionRepo: string
 }
 const dataApi: MockData[] = [
     {
@@ -28,23 +30,27 @@ const dataApi: MockData[] = [
 
 
 
-export const UserListView = ({ width, height }: Props) => {
+export const UserListView = ({ width, height, nameRepo, descriptionRepo }: Props) => {
     return (
-        <FlatList
-            data={dataApi}
-            renderItem={({ item, index }) =>
-                <View style={style.item}>
-                    <UserItemList
-                        key={index}
-                        title={item.name}
-                        source={item.avatar}
-                        height={height}
-                        width={width}
-                    />
-                </View>
+        <View style={style.container}>
+            <Text style={style.title}>{nameRepo}</Text>
+            <Text style={style.description}>{descriptionRepo}</Text>
+            <FlatList
+                data={dataApi}
+                renderItem={({ item, index }) =>
+                    <View style={style.item}>
+                        <UserItemList
+                            key={index}
+                            title={item.name}
+                            source={item.avatar}
+                            height={height}
+                            width={width}
+                        />
+                    </View>
 
-            }
-        />
+                }
+            />
+        </View>
     )
 }
 
@@ -52,10 +58,23 @@ export const UserListView = ({ width, height }: Props) => {
 const style = StyleSheet.create({
     container: {
         display: "flex",
-        flex: 1
+        flex: 1,
+        flexDirection: "column"
     },
     item: {
-        marginBottom: 5
+        marginBottom: 5,
+        paddingLeft: 10,
+
+    },
+    title: {
+        fontSize: 25,
+        paddingLeft: 10,
+        marginBottom: 10
+    },
+    description: {
+        fontSize: 15,
+        paddingLeft: 10,
+        marginBottom: 15
 
     }
 })
