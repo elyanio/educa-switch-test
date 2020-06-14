64c82f640e2c94250ccdc50c81fd81f81b0bbe02
Yanier Alfonso <48162982+elyanio@users.noreply.github.com>
2020-06-13 19:24:20 -0500

    Adding Apollo Client Service

--- App.tsx
+++ App.tsx
@@ -7,25 +7,33 @@ import BottomTabNavigator from './navigation/BottomTabNavigator';
 import LinkingConfiguration from './navigation/LinkingConfiguration';
 import { Provider as PaperProvider } from 'react-native-paper';
 import { theme } from './theme';
+
+import { ApolloProvider } from 'react-apollo';
+import makeApolloClient from './ApolloClient';
+
 const Stack = createStackNavigator();
 
 export default function App() {
-  const isLoadingComplete = useCachedResources();
+const isLoadingComplete = useCachedResources();
+
+const client = makeApolloClient();
 
   if (!isLoadingComplete) {
     return null;
   } else {
     return (
-      <PaperProvider theme={theme}>
-        <View style={styles.container}>
-          {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
-          <NavigationContainer linking={LinkingConfiguration}>
-            <Stack.Navigator>
-              <Stack.Screen name="Root" component={BottomTabNavigator} />
-            </Stack.Navigator>
-          </NavigationContainer>
-        </View>
-      </PaperProvider>
+      <ApolloProvider client={client}>
+        <PaperProvider theme={theme}>
+          <View style={styles.container}>
+            {Platform.OS === 'ios' && <StatusBar barStyle="dark-content" />}
+            <NavigationContainer linking={LinkingConfiguration}>
+              <Stack.Navigator>
+                <Stack.Screen name="Root" component={BottomTabNavigator} />
+              </Stack.Navigator>
+            </NavigationContainer>
+          </View>
+        </PaperProvider>
+      </ApolloProvider>
     );
   }
 }
